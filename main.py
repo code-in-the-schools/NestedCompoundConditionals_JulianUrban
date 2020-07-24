77cf29ec4de9c1024b8efe6e394400c5d945b772
thejduman <thejduman@yahoo.com>
2020-07-24 15:32:07 +0000

    Part 3, not finished

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-#lists for even  numbers
+#lists for even and odd numbers
 even = [2, 4, 6, 8, 10]
+odd = [1, 3, 5, 7, 9]
 for i in range(11): #outer loop
   print("outer loop | i = " + str(i))
   for j in range(11): #inner loop
@@ -7,4 +8,8 @@ for i in range(11): #outer loop
     #check the lists to see if the values for i and j are even
     if i in even and j in even: 
       print("i and j are even")
+    #check the lists to see if the values for i and j are even
+    elif i in odd and j in odd:
+      print("i and j are odd")
+
     
